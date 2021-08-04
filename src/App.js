534e030ff4d25e29381c82c40e56f5773e9d71b2
Le Thai Anh <lethaianh26122000@gmail.com>
2021-08-04 12:00:40 +0700

    version 0.2

--- src/App.js
+++ src/App.js
@@ -4,27 +4,49 @@ import Button from "@atlaskit/button";
 import { useCallback, useState, useEffect } from "react";
 import {v4} from "uuid"
 
+const TODO_APP_STORAGE_KEY = 'TODO_APP';
+
 function App() {
 
   const [todoList, setTodoList] = useState([]);
   const [textInput, setTextInput] = useState("");
-  const onTextInputChange = (e) => {
+
+  useEffect(() => {
+    const storageTodoList = localStorage.getItem(TODO_APP_STORAGE_KEY);
+    if(storageTodoList){
+      setTodoList(JSON.parse(storageTodoList));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(TODO_APP_STORAGE_KEY, JSON.stringify(todoList));
+  }, [todoList]);
+
+  const onTextInputChange = useCallback((e) => {
     setTextInput(e.target.value);
-  }
-  const onAddBtnClick = (e) => {
-    setTodoList([...todoList, {id: v4(), name: textInput, isCompleted: false}, ...todoList,]);
-  }
+  }, []);
+  const onAddBtnClick = useCallback((e) => {
+    setTodoList([ 
+      {id: v4(), name: textInput, isCompleted: false}, 
+      ...todoList,
+    ]);
+    setTextInput("");
+  }, [textInput, todoList]);
+
+  const onCheckBtnClick = useCallback((id) =>{
+    setTodoList(prevState => prevState.map(todo => todo.id == id? {...todo, isCompleted: true}: todo))
+  }, []);
 
   return (
     <>
-    <h3>Danh sách cần làm</h3>
+    <h2>Danh sách cần làm</h2>
     <Textfield name="add-todo" placeholder="Thêm việc cần làm..." elemAfterInput={
       <Button isDisabled={!textInput} appearance='primary' onClick={onAddBtnClick}>Thêm</Button>
     } css={{padding: '2px 4px 2px'}}
       value={textInput}
       onChange={onTextInputChange}
     ></Textfield>
-    <TodoList todoList={todoList} />
+    <TodoList todoList={todoList} onCheckBtnClick={onCheckBtnClick}/>
     </>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import Todo from './Todo'
 
-export default function TodoList({ todoList }) {
+export default function TodoList({ todoList, onCheckBtnClick }) {
     return (
         <>
         {
             todoList.map((todo) => (
-            <Todo todo={todo}/>
+            <Todo key={todo.id} todo={todo} onCheckBtnClick={onCheckBtnClick}/>
             ))}
         </>
     );
